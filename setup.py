d295ef6864c863522f93cf155356fbba5bf2a94b
STORM <dhers.santiago@gmail.com>
2024-11-30 12:23:35 -0300

    packaged rainstorm

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
-# STORM - Simple Tracker for Object Recognition Memory
+# RAINSTORM: Real & Artificial Intelligence Networks – Simple Tracker for Object Recognition Memory
 # Authors: Santiago D'hers. 
 # © 2024. This project is openly licensed under the MIT License.
-# https://github.com/simple-torm/STORM
+# https://github.com/sdhers/RAINSTORM
 
 from setuptools import setup, find_packages
 
@@ -9,14 +9,14 @@ with open("README.md", encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
-    name="simple-torm",
-    version="0.1.0",
+    name="rainstorm",
+    version="0.0.1",
     author="Santiago D'hers",
-    author_email="simple.torm@gmail.com",
-    description="Simple Tracker for Object Recognition Memory",
+    author_email="dhers.santiago@gmail.com",
+    description="Real & Artificial Intelligence Networks – Simple Tracker for Object Recognition Memory",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/simple-torm/STORM",
+    url="https://github.com/sdhers/RAINSTORM",
     packages=find_packages(), # Automatically finds package directories
     install_requires=[
         "numpy",
@@ -29,5 +29,5 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.9',
+    python_requires='>=3.10',
 )
